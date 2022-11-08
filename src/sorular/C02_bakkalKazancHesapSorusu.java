7fc8c6d3c4b19e0c065efe87e4f96beb8fdf0d49
YavuzSelimHasturk <yavuzselimhasturk@gmail.com>
2022-11-08 12:26:50 +0300

    Bakkal Kazancı

--- src/sorular/C02_bakkalKazancHesapSorusu.java
+++ src/sorular/C02_bakkalKazancHesapSorusu.java
@@ -7,22 +7,10 @@ import java.util.Scanner;
 public class C02_bakkalKazancHesapSorusu {
     /*
     Bir bakkalın 7 günlük tüm kazançlarını günlük olarak gösteren bir program
-  yazınız. Ayrıca bakkalın bu hafta ortalama kazandığı miktarı gösteren bir
-  method yazınız. Ayrıca bakkalın hangi günler ortalamanın üstüne kazandığını
-  gösteren bir method yazınız. Ayrıca bakkalın hangi günler ortalamanın altında
-  kazandığını gösteren bir method yazınız.
-
- * 1. Adım : Günleri içeren bir tane ArrayList oluşturun. (gunler)
- * 2. Adım : Günlük kazançları ekleyeceğimiz bir tane ArrayList oluşturun.(gunlukKazanclar)
- * 3. Adım : While döngüsü ile kullanıcıdan 7 günlük kazançları tekek teker alıp gunlukKazanclar ArrayList'e ekle.
- * 4. Adım : getOrtalamaKazanc() adlı method ile ortalama kazancı alın.
- * 5. Adım : getOrtalamaninUstundeKazancGünleri() adlı method oluşturun. for döngüsü ile
-             tüm günleri ortalama kazanç ile karşılaştır ortalama kazançtan yüksekse o günleri return yap.
- * 6. Adım : getOrtalamaninAltindaKazancGünleri() adlı method oluşturun. for döngüsü ile tüm günleri ortalama
-             kazanç ile karşılaştır ortalama kazançtan aşağıysa o günleri return yap.
+  yazınız. Ayrıca bakkalın bu hafta ortalama kazandığı miktarı, bakkalın hangi günler ortalamanın
+  üstüne kazandığını, bakkalın hangi günler ortalamanın altında kazandığını göstersin...
      */
     public static void main(String[] args) {
-
 
         List<String> haftaninGunleri = new ArrayList<>();
         haftaninGunleri.add("Pazartesi");
@@ -48,13 +36,34 @@ public class C02_bakkalKazancHesapSorusu {
             gunler++;
             sayac--;
         }
-        double toplamKazanc= 0;
 
+        double toplamKazanc= 0;
         for (int i = 0; i <gunlukKazanclar.size() ; i++) {
-                toplamKazanc+=gunlukKazanclar.get(i);
+            toplamKazanc+=gunlukKazanclar.get(i);
         }
+
+        double ortalamaKazanc = toplamKazanc/haftaninGunleri.size();
+
+        List<String> ortalamaKazancUstuGun = new ArrayList<>();
+        for (int i = 0; i <gunlukKazanclar.size() ; i++) {
+
+            if (gunlukKazanclar.get(i)>ortalamaKazanc){
+                ortalamaKazancUstuGun.add(haftaninGunleri.get(i));
+            }
         }
+        List<String> ortalamaKazancAltiGun = new ArrayList<>();
+        for (int i = 0; i <gunlukKazanclar.size() ; i++) {
+            if (gunlukKazanclar.get(i)<ortalamaKazanc){
+                ortalamaKazancAltiGun.add(haftaninGunleri.get(i));
+            }
+        }
+        System.out.println(haftaninGunleri);
+        System.out.println("Kazanc Listesi :\n " + haftaninGunleri +"\n\t" +gunlukKazanclar);
+        System.out.println("Ortalama Kazanc : " + ortalamaKazanc);
+        System.out.println("Ortalama Ustu Kazanc Gunleri : " + ortalamaKazancUstuGun);
+        System.out.println("Ortalama Alti Kazanc Gunleri : " + ortalamaKazancAltiGun);
     }
+}
 
 
 
